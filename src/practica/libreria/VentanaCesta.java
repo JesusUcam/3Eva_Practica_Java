e9a917117269f6004c2e36cb3f272287e0a6e19a
292802 <292802@MUA05ALPSP04108.aulas.ucam.edu>
2023-05-30 14:17:15 +0200

    jesulandro

--- src/practica/libreria/VentanaCesta.java
+++ src/practica/libreria/VentanaCesta.java
@@ -2,6 +2,7 @@ package practica.libreria;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
@@ -14,158 +15,159 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-public class VentanaCesta extends JFrame implements ActionListener{
+public class VentanaCesta extends JFrame implements ActionListener {
 
-	private int contPP=0;
+	private int contPP = 0;
 	private double total = 0;
-	
+
 	private JPanel jp, jpScroll;
-	
+
 	private JScrollPane jsp;
-	
+
 	private JButton comprar;
-	
+
 	public VentanaCesta() {
-		
+
 		Cliente c = Cliente.c;
-		
+
 		setSize(640, 360);
-		
-		//Añado el panel para los productos
+
+//Añado el panel para los productos
 		jp = new JPanel();
-		jp.setBackground(new Color(215, 215, 250) );
+		jp.setBackground(new Color(215, 215, 250));
 		add(jp);
 		setContentPane(jp);
-		
+
 		jpScroll = new JPanel();
 		jpScroll.setLayout(null);
-		
+
 		add(jpScroll);
-		
+
 		jsp = new JScrollPane(jpScroll);
-		
-		if (contPP<10) {
-		
-			jsp.setBounds(20,20,350,contPP*25);
-			
-		}else {
-			
-			jsp.setBounds(20,20,350,250);
-			
+
+		if (contPP < 10) {
+
+			jsp.setBounds(20, 20, 350, contPP * 25);
+
+		} else {
+
+			jsp.setBounds(20, 20, 350, 250);
+
 		}
 		add(jsp);
-		
-		comprar = new JButton("Comprar todo");
+
+		comprar = new JButton("Pagar");
+		comprar.setFont(new Font("Arial", Font.BOLD, 16));
 		add(comprar);
-		
-		comprar.setBounds(450,50,150,50);
-		
+
+		comprar.setBounds(450, 240, 150, 50);
+
 		pintarProductosCesta();
-		
-		if (contPP<10) {
-		
-			jsp.setBounds(20,20,350,(contPP*25)+3);
-			
-		}else {
-			
-			jsp.setBounds(20,20,365,250);
-			
+
+		if (contPP < 10) {
+
+			jsp.setBounds(20, 20, 350, (contPP * 25) + 3);
+
+		} else {
+
+			jsp.setBounds(20, 20, 365, 250);
+
 		}
-		
+
 		comprar.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
 
-				int nuevoCliente = JOptionPane.showConfirmDialog(null, "Esta a punto de hacer un pago de "+String.format("%.2f", total)+"€, ¿Desea continuar?", "Realizar pago", JOptionPane.YES_NO_OPTION);
+				int nuevoCliente = JOptionPane.showConfirmDialog(null,
+						"Esta a punto de hacer un pago de " + String.format("%.2f", total) + "€, ¿Desea continuar?",
+						"Realizar pago", JOptionPane.YES_NO_OPTION);
 				if (nuevoCliente == JOptionPane.YES_OPTION) {
 					c.getCesta().clear();
 					JOptionPane.showMessageDialog(null, "Gracias por su compra");
 					dispose();
 				}
-		
 			}
-		
 		});
-		
+
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setLayout(null);
 		setVisible(true);
-		
+
 	}
-	
+
 	public void pintarProductosCesta() {
-		
+
 		for (Producto cesta : Cliente.c.getCesta()) {
-		
+
 			JButton jbProducto = crearProductosCesta(cesta);
 			total += cesta.getPrecioUnidad();
-			
+
 		}
 
-		jpScroll.setPreferredSize(new Dimension(0,contPP*25));
-		
-		JLabel jlTotal = new JLabel(String.format("%.2f", total)+"€");
+		jpScroll.setPreferredSize(new Dimension(0, contPP * 25));
+		JLabel jlTotal = new JLabel(String.format("Precio total: %.2f", total) + "€");
 		add(jlTotal);
-		jlTotal.setBounds(300,200,120,30);
-	
+		jlTotal.setBounds(460, 200, 150, 30);
+		jlTotal.setFont(new Font("Arial", Font.BOLD, 16));
+
 	}
-	
+
 	public JButton crearProductosCesta(Producto p) {
-		
+
 		/*
 		 * 640x360
 		 */
-		
-		int y = 0 + (25*contPP); 
-				
+
+		int y = 0 + (25 * contPP);
+
 		JButton jbProducto = new JButton(p.getNombre());
 		JButton jbEliminar = new JButton("Eliminar");
 		jpScroll.add(jbProducto);
 		jpScroll.add(jbEliminar);
-		
-		jbProducto.setBounds(0,y,250,25);
-		jbEliminar.setBounds((250),y,100,25);
-		
+
+		jbProducto.setBounds(0, y, 250, 25);
+		jbProducto.setFont(new Font("Arial", Font.BOLD, 15));
+		jbEliminar.setBounds((250), y, 100, 25);
+		jbEliminar.setFont(new Font("Arial", Font.BOLD, 15));
+
 		contPP++;
-		
+
 		jbProducto.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-		
+
 				VentanaProducto vp = new VentanaProducto(p);
-				
+
 			}
-		
+
 		});
-		
+
 		jbEliminar.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				
+
 				Cliente.c.getCesta().remove(p);
-				p.setStock(p.getStock()+1);
+				p.setStock(p.getStock() + 1);
 				jsp.remove(jbProducto);
 				dispose();
-				
-				if (Cliente.c.getCesta().size()>0) {
-					
+
+				if (Cliente.c.getCesta().size() > 0) {
+
 					VentanaCesta nVC = new VentanaCesta();
-					
+
 				}
-				
+
 			}
-		
+
 		});
-		
-		return jbProducto; //No necesario
-		
+
+		return jbProducto; // No necesario
+
 	}
-	
+
 	public void actionPerformed(ActionEvent arg0) {
-		
-		
-		
+
 	}
 
 }
